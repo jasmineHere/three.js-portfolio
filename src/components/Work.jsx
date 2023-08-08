558a915d6e597df1bab316fbc1fe757c12b3d0eb
Jasmine Muman <jasminemuman21@gmail.com>
2023-08-08 13:42:31 -0400

    added components

---
+++ src/components/Work.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`
+
+function Work() {
+  return (
+    <Section>Work</Section>
+  )
+}
+
+export default Work
