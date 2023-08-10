346e21ab312af73ad2acfb22782e0e598b0ebe43
Jasmine Muman <jasminemuman21@gmail.com>
2023-08-09 23:41:23 -0400

    Added changes to Hero-section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,27 @@ import Navbar from './Navbar'
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+  dsiplay: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`; 
 
-function Hero() {
+const Container = styled.div`
+    height: 100vh;
+    scroll-snap-align: center;
+    width: 1100px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Hero = () => {
   return (
     <Section>
         <Navbar/>
+        <Container> Hero </Container>
     </Section>
   )
-}
+};
 
-export default Hero
+export default Hero;
