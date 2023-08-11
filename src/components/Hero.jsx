a5255ed4b8d918a804c7baa0e2a05520869f1624
Jasmine Muman <jasminemuman21@gmail.com>
2023-08-10 21:57:01 -0400

    added changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Section = styled.div`
   dsiplay: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
 `; 
 
 const Container = styled.div`
@@ -19,11 +19,53 @@ const Container = styled.div`
     justify-content: space-between;
 `;
 
+const Left = styled.div`
+    
+`;
+const Title = styled.h1`
+    
+`;
+const WhatWeDo = styled.div`
+    
+`;
+const Line = styled.img`
+    
+`;
+const Subtitle = styled.h2`
+    
+`;
+const Desc = styled.p`
+    
+`;
+const Button = styled.button`
+    
+`;
+
+const Right = styled.div`
+    
+`;
+const Img = styled.img`
+    
+`;
+
 const Hero = () => {
   return (
     <Section>
         <Navbar/>
-        <Container> Hero </Container>
+        <Container>
+            <Left>
+                <Title>Think. Make. Solve</Title>
+                <WhatWeDo>
+                    <Line src = "./img/line.png"/>
+                    <Subtitle></Subtitle>
+                </WhatWeDo>
+                <Desc>we enjoy creating delightful, human-centered digital experiences.</Desc>
+                <Button>Learn More</Button>
+            </Left>
+            <Right>
+                <Img src = "./img/moon.png"/>
+            </Right>
+        </Container>
     </Section>
   )
 };
