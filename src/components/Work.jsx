dca309333916abcbbe0079f7c78a567f3b4e4eb9
Jasmine Muman <jasminemuman21@gmail.com>
2023-08-15 01:09:16 -0400

    added text animation to Work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -14,42 +14,63 @@ const Section = styled.div`
   scroll-snap-align: center;
   display: flex;
   justify-content: center;
-`
+`;
+
 const Container = styled.div`
   width: 1100px;
   display: flex;
   justify-content: space-between;
 
-`
+`;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
-`
+`;
+
 const List = styled.ul`
   list-style: none;
   display: flex;
   flex-direction: column;
   gap: 20px;
-`
+`;
+
 const ListItem = styled.li`
-  font-size: 74px;
+  font-size: 70px;
   font-weight: bold;
   cursor: pointer;
   color: transparent;
   -webkit-text-stroke: 1px white;
-  psoition: relative;
+  position: relative;
 
-  ::after {
+  &:after {
     content: "${(props) => props.text}";
     position: absolute;
     top: 0;
     left: 0;
+    color: pink;
+    width: 0px;
+    overflow: hidden;
+    white-space: nowrap;
   }
-`
+
+  &:hover {
+    &:after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to{
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
+
 const Right = styled.div`
   flex: 1;
-`
+`;
 
 function Work() {
   return (
@@ -58,8 +79,8 @@ function Work() {
         <Left>
           <List>
             {data.map((item) => (
-              <ListItem key={item} text={item}>{item}</ListItem>
-            ))}
+              <ListItem key={item} text={item}> {item} </ListItem>
+            ))} 
           </List>
         </Left>
         <Right></Right>
