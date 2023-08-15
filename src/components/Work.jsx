f94fa5ff7d1cbcba0f89f842bea7e938a1761e23
Jasmine Muman <jasminemuman21@gmail.com>
2023-08-14 23:51:43 -0400

    added Work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,14 +1,70 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media",
+];
+
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`
+const Container = styled.div`
+  width: 1100px;
+  display: flex;
+  justify-content: space-between;
+
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+const ListItem = styled.li`
+  font-size: 74px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  psoition: relative;
+
+  ::after {
+    content: "${(props) => props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`
+const Right = styled.div`
+  flex: 1;
 `
 
 function Work() {
   return (
-    <Section>Work</Section>
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item} text={item}>{item}</ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
   )
 }
 
